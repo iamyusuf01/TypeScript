ba4543949cd1721f86aa132b36d05cd803ee8017
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2025-12-22 13:07:10 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `hello ${person}, welcome to chaiaurcode`;
+}
+const username = "Chai aur TypeScript";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(person: string): string {
+    return `hello ${person}, welcome to chaiaurcode`
+}
+
+const username: string = "Chai aur TypeScript"
+console.log(greet(username));
