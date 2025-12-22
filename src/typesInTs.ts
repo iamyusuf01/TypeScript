58b9d6634158534f23907f22fddaceedf16ad8c1
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2025-12-22 13:22:18 +0530

    Type Annotation and Inference

---
+++ src/typesInTs.ts
@@ -0,0 +1,10 @@
+let drink = "chai";
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+let channelName = 'Chaiaurcode'
+
+let chaiFlavour: string = "masala chai"
+chaiFlavour = 'Ginger tea'
+
+let chaiOrder:boolean
