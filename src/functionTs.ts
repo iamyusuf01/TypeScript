3aaae20c70fb82a837b3d9b94f12b36341b638a8
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2026-01-06 15:14:56 +0530

    functions

---
+++ src/functionTs.ts
@@ -0,0 +1,26 @@
+function makeChai(type:string, cups:number){
+    console.log(`Making ${cups} cups of ${type}`)
+}
+
+makeChai('Masala', 2)
+
+function getChaiPrice(): number {
+    return 24
+}
+
+function makeOrder(order: string){
+    if(!order) return null
+    return order
+}
+
+function logChai(): void {
+    console.log('Chai is ready')
+}
+
+function createChai(order: {
+    type:string;
+    sugar:number;
+    size: 'small' | 'large'
+}): number {
+    return 4
+}
