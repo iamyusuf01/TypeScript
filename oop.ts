e58f746d9ecbe30d96ca96277eecb0214dd42a35
Md Yusuf Ansari <mdyusuf1202@gmail.com>
2026-01-08 12:29:22 +0530

    public private protected and access modifier

---
+++ oop.ts
@@ -0,0 +1,105 @@
+// class Chai {
+//     flavour: string;
+//     price: number
+
+//     // constructor(flavour: string, price:number){
+//     //     this.flavour = flavour
+//     //     this.price = price
+//     // }
+//     constructor(flavour: string, price: number) {
+//         this.flavour = flavour
+//         console.log(this)
+//     }
+
+// }
+
+// const masalaChai = new Chai('Ginger')
+// masalaChai.flavour = 'masala'
+
+// {Access Modifier }
+
+class Chai {
+    public flavour: string = 'Masala'
+
+    private secretIngredients = "Cardamom"
+
+    reveal() {
+        return this.secretIngredients
+    }
+
+}
+
+class Shop {
+    protected shopName = "Chai corner"
+}
+
+class Branch extends Shop {
+    getName() {
+        return this.shopName
+    }
+}
+
+class Wallet {
+    #balance = 100
+
+    getBalance() {
+        return this.#balance
+    }
+}
+
+const w = new Wallet()
+
+class Cup {
+    readonly capacity: number = 250
+
+    constructor(capacity: number) {
+        this.capacity = capacity
+    }
+}
+
+class ModernChai {
+    private _sugar = 2
+
+    get sugar() {
+        return this._sugar
+    }
+
+    set sugar(value: number) {
+        if (value > 5) throw new Error('Too sweet');
+        this._sugar = value
+    }
+}
+
+const c = new ModernChai()
+c.sugar = 3
+
+class EkChai {
+    static shopName = "Chaicode caffe"
+
+    constructor(public flavour: string) { }
+
+}
+
+console.log(EkChai.shopName)
+
+abstract class Drink {
+    abstract make(): void
+}
+
+class MyChai extends Drink {
+    make() {
+        console.log("Brewing chai");
+    }
+}
+
+class Heater{
+    heat(){}
+}
+
+class ChaiMaker{
+    constructor(private heater: Heater){}
+
+    make(){
+        this.heater.heat
+    }
+}
